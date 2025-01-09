47b301c3162404e3d3d605503f27de6b3613e294
sudeepmaari <sudeepk0406@gmail.com>
2025-01-09 18:22:55 +0530

    Registration.java (signup(),login()) added.

---
+++ Food/src/main/java/com/model/Registration.java
@@ -0,0 +1,87 @@
+package com.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import jakarta.servlet.http.HttpSession;
+
+public class Registration {
+	
+	private Connection con;
+    HttpSession se;
+
+    public Registration(HttpSession session) {
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver"); // load the drivers
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/delicious", "root", "tiger");
+            // connection with data base
+            se = session;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public String Registration(String name, String phone, String email, String pw) {
+        PreparedStatement ps;
+        String status = "";
+        try {
+            Statement st = null;
+            ResultSet rs = null;
+            st = con.createStatement();
+            rs = st.executeQuery("select * from customer where phone='" + phone + "' or email='" + email + "';");
+            boolean b = rs.next();
+            if (b) {
+                status = "existed";
+            } else {
+                ps = (PreparedStatement) con.prepareStatement("insert into customer values(0,?,?,?,?)");
+                ps.setString(1, name);
+                ps.setString(2, phone);
+                ps.setString(3, email);
+                ps.setString(4, pw);
+                int a = ps.executeUpdate();
+                if (a > 0) {
+                    status = "success";
+                } else {
+                    status = "failure";
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return status;
+    }
+    
+    public String login(String email, String pass) {
+        String status1 = "", id = "";
+        String name = "", emails = "";
+
+        try {
+            Statement st = null;
+            ResultSet rs = null;
+            st = con.createStatement();
+
+            rs = st.executeQuery("select * from customer where email='" + email + "' and password='" + pass + "';");
+            boolean b = rs.next();
+            if (b == true) {
+                id = rs.getString("slno");
+                name = rs.getString("name");
+                emails = rs.getString("email");
+                se.setAttribute("uname", name);
+                se.setAttribute("email", emails);
+                se.setAttribute("id", id);
+                status1 = "success";
+            } else {
+                status1 = "failure";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return status1;
+    }
+}

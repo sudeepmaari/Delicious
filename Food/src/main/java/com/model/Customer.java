9f18397074f18e64e5143568b12cfc49ae4a2559
sudeepmaari <sudeepk0406@gmail.com>
2025-01-09 16:19:44 +0530

    Customer dto added

---
+++ Food/src/main/java/com/model/Customer.java
@@ -0,0 +1,50 @@
+package com.model;
+
+public class Customer {
+	String cid;
+	String name;
+	String phone;
+	String email;
+	String password;
+	String lid;
+	
+	
+	public String getCid() {
+		return cid;
+	}
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+	public String getFname() {
+		return name;
+	}
+	public void setFname(String fname) {
+		this.name = fname;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLid() {
+		return lid;
+	}
+	public void setLid(String lid) {
+		this.lid = lid;
+	}
+	
+	
+}
